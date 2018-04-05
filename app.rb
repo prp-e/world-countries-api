8e17785f4dce072374d388ddb7551b16964485e8
Muhammadreza Haghiri <haghiri75@gmail.com>
2018-04-05 18:07:19 +0430

    API codes :)

--- app.rb
+++ app.rb
@@ -7,9 +7,16 @@ end
 
 class Country 
     include Mongoid::Document
+    field :id, type: String #Using TLD
     field :name, type: String 
     field :official_name, type: String 
     field :gov_type, type: String 
     field :calling_code, type: String 
     field :tld , type: String 
 end
+
+get '/' do 
+    @countries = Country.all 
+    @title = "Countries List"
+    erb :index 
+end 
